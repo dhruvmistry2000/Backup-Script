97868a2c7deca218db9365fa818b68f187592f34
Dhruv Mistry <dhruvmistry2000@gmail.com>
2025-01-28 18:02:07 +0530

    Gdrive Backup added

--- backup.sh
+++ backup.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-
-SOURCE_DIR="/home/dhruv/Github/mylinux"      # Directory to backup
-BACKUP_DIR="/home/dhruv/Document"     # Where backups will be stored
+SOURCE_DIR="/home/dhruv/Github/mylinux"    # Directory to backup - source directory path  
+BACKUP_DIR="/home/dhruv/Document"     # Where backups will be stored locally
+REMOTE_NAME="gdrive"                  # Name of rclone remote
+REMOTE_DIR="/backup"                  # Remote directory path
 
 # Basic error checking
 if [ -z "$SOURCE_DIR" ]; then
@@ -16,7 +17,7 @@ fi
 
 # Create backup directory
 if [ ! -d "$BACKUP_DIR" ]; then
-    echo "Make backup directory"        
+    echo "Making backup directory"        
     mkdir "$BACKUP_DIR"
 fi
 
@@ -24,13 +25,22 @@ fi
 TIMESTAMP=$(date +%H%M%S_%d%m%Y)
 BACKUP_FILE="backup_${TIMESTAMP}.zip"
 
-# Create the backup
-echo "Starting backup process..."
-zip -r "${BACKUP_DIR}/${BACKUP_FILE}" "$SOURCE_DIR" > backup.log
-
+# Create the local backup using zip
+echo "Starting local backup process..."
+zip -r "${BACKUP_DIR}/${BACKUP_FILE}" "$SOURCE_DIR" > local.log
 if [ $? -eq 0 ]; then
-    echo "Backup completed: ${BACKUP_DIR}/${BACKUP_FILE}" 
+    echo "Local Backup completed: ${BACKUP_DIR}/${BACKUP_FILE}" 
 else
-    echo "Backup failed!"
+    echo "Local Backup failed!"
+    exit 1
+fi
+
+# Copy backup to remote storage using rclone
+echo "Starting remote backup process..."
+rclone copy "${BACKUP_DIR}/${BACKUP_FILE}" "$REMOTE_NAME:$REMOTE_DIR" > remote.log
+if [ $? -eq 0 ]; then
+    echo "Remote Backup completed: ${BACKUP_DIR}/${BACKUP_FILE}" 
+else
+    echo "Remote Backup failed!"
     exit 1
 fi
